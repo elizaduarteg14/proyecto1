bbd353d6ecfecb4a3244133c8eb545069f940ebf
elizaduarteg14 <00305222@uca.edu.sv>
2026-06-21 17:47:26 -0600

    Comment out module imports in app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from login import login
+#from login import login
 #from modulos.clientes import clientes
 #from modulos.productos import productos
 #from modulos.ventas import ventas
