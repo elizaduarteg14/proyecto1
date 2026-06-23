6b95ede363734c49b1760c58ff1cde56e453b78e
elizaduarteg14 <00305222@uca.edu.sv>
2026-06-23 00:36:20 -0600

    Update conexion.py

--- config/conexion.py
+++ config/conexion.py
@@ -2,9 +2,9 @@ import pymysql
 
 def conectar():
     return pymysql.connect(
-        host="localhost",
-        user="root",
-        password="FASdel47!",
-        database="tarea3",
+        host="bwzzc4ivpfvrmnb3g3qo-mysql.services.clever-cloud.com",
+        user="ucr379ul46b7eqsg",
+        password="2uz9cVAR5DYRoYWqmZay",
+        database="bwzzc4ivpfvrmnb3g3qo",
         port=3306
     )
